37d871b4f2537ee8c4ee1064f44054feb932c2b1
Sergiu Guglea <sguglea@gmail.com>
2024-04-01 16:22:14 +0300

    add joke ai result image

--- JokeAI.Web/obj/Debug/net8.0/JokeAI.Web.AssemblyInfo.cs
+++ JokeAI.Web/obj/Debug/net8.0/JokeAI.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokeAI.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de8b2364da12f0caec3f81d187f7afe4f8942f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokeAI.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokeAI.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
